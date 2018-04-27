4290f06554f1255a1b0e2dec4e929b2989d27064
Sarah Zhao <sarahzhao25@gmail.com>
2018-04-27 18:05:55 -0400

    added student model

--- w2/db/models/student.js
+++ w2/db/models/student.js
@@ -3,6 +3,32 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define(
+  'student',
+  {
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    fullName: {
+      type: Sequelize.VIRTUAL,
+      get() {
+        return `${this.firstName} ${this.lastName}`;
+      }
+    }
+  },
+  {}
+);
 
 module.exports = Student;
