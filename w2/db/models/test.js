4849d8243553b9725e70d4a6cdc6d50dacddc442
Sarah Zhao <sarahzhao25@gmail.com>
2018-04-27 18:09:01 -0400

    added cycle3 end pt

--- w2/db/models/test.js
+++ w2/db/models/test.js
@@ -3,6 +3,35 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define();
+const Test = db.define('test', {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  grade: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+Test.passing = function() {
+  return Test.findAll({
+    where: {
+      grade: {
+        [Op.gt]: 70
+      }
+    }
+  });
+};
+
+Test.findBySubject = function(type) {
+  return Test.findAll({
+    where: {
+      subject: type
+    }
+  });
+};
+
+Test.belongsTo(Student, { as: 'student' });
 
 module.exports = Test;
