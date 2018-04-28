b17a6fd2c9aec2924490c1f4914f26e2e846a55e
Chiara <chiara@GAs-MacBook-Air.local>
2018-04-28 11:40:44 -0400

    passing first 5 specs

--- w2/db/models/student.js
+++ w2/db/models/student.js
@@ -1,8 +1,24 @@
 'use strict'
 
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require('sequelize')
+const db = require('../db')
 
-const Student = db.define();
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+})
 
-module.exports = Student;
+module.exports = Student
